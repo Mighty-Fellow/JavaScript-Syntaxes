38c29ea7db40604d8701739627e85ad09447a315
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2020-11-15 20:04:36 +0600

    Concate

---
+++ 9.Concate/pg.js
@@ -0,0 +1,10 @@
+var number1 = [1,2,3,4,5,6,7]
+var number2 = [10,20,30,40,50,60,70]
+
+var result = number1.concat(number2)
+// var result = number1.concat(number2,number3,number4)
+
+console.log(number1);// in this method they doesn't change main array 
+console.log(number2);// in this method they doesn't change main array 
+
+console.log(result);
